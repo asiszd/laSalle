e9ceab6f1c796c40146082e8ad7b9658559ab2cd
asis <asisduzi@gmail.com>
2019-12-20 05:14:49 -0600

    Nuevo Usuario

--- grails-app/assets/javascripts/lasalle/controllers/principalController.js
+++ grails-app/assets/javascripts/lasalle/controllers/principalController.js
@@ -20,6 +20,7 @@ angular
 
 function PrincipalController(Principal, $http, $filter,$window) {
     var vm = this;
+    var todosGrailsServerUri = 'http://localhost:8080/principales';
 
     vm.authenticated = false;
     vm.user = {};
@@ -29,6 +30,34 @@ function PrincipalController(Principal, $http, $filter,$window) {
     vm.nivel = [{"text":"Bachillerato"}, {"text":"Licenciatura"}, {"text":"Maestria"}, {"text":"Doctorado"}];
     vm.filteredItems = [];
     vm.principales = [];
+    vm.especialidades = {especialidad: ''};
+    vm.niveles = [{
+        "nivel":"Bachillerato",
+        "especialidades":[{
+        }]
+    }, {
+        "nivel":"Licenciatura",
+        "especialidades":[{
+            "name": "Ingeniería de Software y Sistemas Computacionales",
+            "name": "Educación",
+            "name": "Negocios Internacionales",
+            "name": "Arquitectura",
+            "name": "Psicología"
+        }]
+    }, {
+        "nivel": "Maestría",
+        "especialidades": [{
+            "name": "Tecnologías de la Información Empresarial",
+            "name": "Educación y Docencia",
+            "name": "Investigación Educativa"
+        }]
+    }, {
+        "nivel": "Doctorado",
+        "especialidades": [{
+            "name": "Doctorado en Derecho",
+            "name": "Doctorado en Educación"
+        }]
+    }];
     //vm.principales = Principal.list();
     var baseURL = 'localhost:8080/';
 
@@ -51,6 +80,7 @@ function PrincipalController(Principal, $http, $filter,$window) {
     };
 
     vm.save = function() {
+        console.log(vm.newPrincipal)
         vm.newPrincipal.$save({}, function() {
             vm.principales.push(angular.copy(vm.newPrincipal));
             vm.newPrincipal = new Principal();
@@ -72,4 +102,16 @@ function PrincipalController(Principal, $http, $filter,$window) {
     vm.toggleDisplay = function() {
         vm.isShow = !vm.isShow;
     }
+
+    vm.getEspecialidades = function (selectedNivel) {
+        var filtroEspe = $filter('filter')(vm.niveles, selectedNivel);
+        return filtroEspe[0].especialidades;
+    }
+
+    vm.addTodo = function(principal) {
+        var todo = {name: "This is a new todo.", completed: false};
+        $http.post(todosGrailsServerUri,todo);
+        console.log(principal);
+    };
+
 }
